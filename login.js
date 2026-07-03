0893a88d3a835a3e9843cd64621cdd92bab57507
ramkumaryadavmafia-oss <ramkumaryadav.mafia@gmail.com>
2026-07-03 10:51:55 +0530

    Update login.js
    Signed-off-by: ramkumaryadavmafia-oss <ramkumaryadav.mafia@gmail.com>

--- login.js
+++ login.js
@@ -1,44 +1,31 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { auth } from "./firebase.js";
+
 import {
-  getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAAHlv466bMEtiCf_Z0fHA9wgFfcQgqEsg",
-  authDomain: "my-ai-app-cdfce.firebaseapp.com",
-  projectId: "my-ai-app-cdfce",
-  storageBucket: "my-ai-app-cdfce.firebasestorage.app",
-  messagingSenderId: "306306694555",
-  appId: "1:306306694555:web:90372c9f6d970544acf53e",
-  measurementId: "G-SQHZNR1QK0"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
 window.signup = async function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
 
   try {
     await createUserWithEmailAndPassword(auth, email, password);
-    alert("✅ Account ban gaya!");
-  } catch (error) {
-    alert(error.message);
+    alert("Signup successful!");
+  } catch (err) {
+    alert(err.message);
   }
 };
 
 window.login = async function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
 
   try {
     await signInWithEmailAndPassword(auth, email, password);
-    alert("✅ Login Successful!");
+    alert("Login successful!");
     window.location.href = "index.html";
-  } catch (error) {
-    alert(error.message);
+  } catch (err) {
+    alert(err.message);
   }
 };
