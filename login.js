4cffdee56efeaf78591f475c9936c02686572cb1
ramkumaryadavmafia-oss <ramkumaryadav.mafia@gmail.com>
2026-07-03 09:38:56 +0530

    Add files via upload
    Signed-off-by: ramkumaryadavmafia-oss <ramkumaryadav.mafia@gmail.com>

---
+++ login.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.signup = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("✅ Account ban gaya!");
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("✅ Login Successful!");
+    window.location.href = "index.html";
+  } catch (error) {
+    alert(error.message);
+  }
+};
