ee2a0d5fb204aa57750bac8e0e53f5f59bf7dbd6
ramkumaryadavmafia-oss <ramkumaryadav.mafia@gmail.com>
2026-07-04 13:22:50 +0530

    Update server.js
    Signed-off-by: ramkumaryadavmafia-oss <ramkumaryadav.mafia@gmail.com>

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require("express");
-const cors = require("cors");
-const { GoogleGenAI } = require("@google/genai");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
+
+dotenv.config();
 
 const app = express();
 
@@ -20,23 +23,35 @@ app.post("/chat", async (req, res) => {
     const { message } = req.body;
 
     const today = new Date().toLocaleDateString("en-IN", {
-  day: "numeric",
-  month: "long",
-  year: "numeric"
-});
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+
+    const prompt = `
+Aaj ki date ${today} hai. Is date ko current date maan kar jawab do.
+
+Tum ek smart AI assistant ho.
+Hamesha simple Hindi me 2-3 line me jawab do.
+Agar user detail maange tabhi lamba jawab dena.
+
+User: ${message}
+`;
 
     const response = await ai.models.generateContent({
       model: "gemini-2.5-flash",
-      contents: "Aaj ki date ${today} hai. Is date ko current date maan kar jawab do.Tum ek smart AI assistant ho. Hamesha simple Hindi me 2-3 line me jawab do. Agar user detail maange tabhi lamba jawab dena. User: " + message,
-});
+      contents: prompt,
+    });
 
     res.json({
       reply: response.text,
     });
+
   } catch (error) {
     console.error(error);
+
     res.status(500).json({
-      reply: "⚠️ Aaj ke liye AI limit khatam ho gayi hai. Kripya thodi der baad ya kal phir try karein.",
+      reply: "⚠️ AI se connect nahi ho pa raha. Thodi der baad phir try karein.",
     });
   }
 });
